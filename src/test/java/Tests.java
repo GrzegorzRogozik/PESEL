b099f67c37e1e8abf5fb12bf4a9adf9b0d257bb0
unknown <rogozik.grzegorz@gmail.com>
2020-03-10 21:57:17 +0100

    add test

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,28 +1,50 @@
+import com.uam.Pesel;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Tests {
     @Test
-    public void peselLenghtTest()
+    public void peselWithLenghtotherthenElevenShouldAlweysReturnFalse()
     {
+        Pesel tester = new Pesel();
         System.out.println("First testcase executed");
-    }
+        Assert.assertEquals(tester.peselValidation("123123"),false);
+        Assert.assertEquals(tester.peselValidation("123456789101"),false);
+        Assert.assertEquals(tester.peselValidation("87090409472"),true);
 
+    }
+// Nr PESEL uwzględnia osoby urodzone tylko między 1900, a 2100 rokiem, wyszedłem z założenia, że na chwile obecną jest to wystarczajaca grupa numerów
     @Test
-    public void peselValidation()
+    public void peselWithWrongMonthNumberSchouldAleysReturnFalse()
     {
+        Pesel tester = new Pesel();
         System.out.println("Second testcase executed");
+        Assert.assertEquals(tester.peselValidation("01431951295"),false);
+        Assert.assertEquals(tester.peselValidation("78002642936"),false);
+        Assert.assertEquals(tester.peselValidation("64032864469"),true);
     }
 
     @Test
-    public void nameValidation()
+    public void nameWithNumberOrSpecialCharShouldAlwaysReturnFalse()
     {
+        Pesel tester = new Pesel();
         System.out.println("Third testcase executed");
+        Assert.assertEquals(tester.nameValidator("!@sasdSASD"),false);
+        Assert.assertEquals(tester.nameValidator("M4rek"),false);
+        Assert.assertEquals(tester.nameValidator("Damian"),true);
+        Assert.assertEquals(tester.nameValidator("AnDrZej"),true);
     }
 
     @Test
-    public void cityValidation()
+    public void cityWithNumberOrSpecialCharShouldAlwaysReturnFalse()
     {
+        Pesel tester = new Pesel();
         System.out.println("Another testcase executed");
+        Assert.assertEquals(tester.cityValidator("!@sasdSASD"),false);
+        Assert.assertEquals(tester.cityValidator("12334"),false);
+        Assert.assertEquals(tester.cityValidator("Poznan"),true);
+        Assert.assertEquals(tester.cityValidator("W4rszawa"),false);
+        Assert.assertEquals(tester.cityValidator("GdYnIa"),true);
     }
 }
 
